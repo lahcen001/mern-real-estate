e6a24fcdd4eb5cba92fbdb8b8dfdea0e83736a05
lahcen001 <lahcen0000lh@gmail.com>
2023-12-18 02:51:02 -0500

    upload image fonctionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,21 @@ import SignUp from './pages/SignUp'
 import Signin from './pages/Signin'
 import About from './pages/About'
 import Header from './components/Header'
+import Profile from './pages/Profile'
+import PrivateRoute from './components/PrivateRoute'
 export default function App() {
   return (
    <BrowserRouter>
    <Header/>
    <Routes>
     <Route path='/' element={<Home/>}/>
-    <Route path='/sign-up' element={<SignUp/>} />
-    <Route path='/sign-in' element={<Signin/>}/>
+    <Route path='/signup' element={<SignUp/>} />
+    <Route path='/signin' element={<Signin/>}/>
     <Route path='/about' element={<About/>}/>
-    <Route/>
+    <Route element={<PrivateRoute/>}>
+    <Route path='/profile' element={<Profile/>}/><Route/>
+    </Route>
+    
     
    
    </Routes>
