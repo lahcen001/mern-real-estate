d4e27a01adf2e2954440f7a7bd22c4f17c4405db
lahcen001 <lahcen0000lh@gmail.com>
2024-01-01 04:01:30 -0500

    upload image listing

--- src/pages/CreateListining.jsx
+++ src/pages/CreateListining.jsx
@@ -1,6 +1,90 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
 export default function CreateListining() {
+  const [file, setFile] = useState([]);
+  const [uploadImageError, setUploadImageError] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [uploadImage, setUploadImage] = useState(false);
+  const [formdata, setFormData] = useState({
+    imagesUrl: [],
+  });
+
+  const handleImageUpload = (e) => {
+    setUploadImage(true);
+    setUploadImageError("");
+    e.preventDefault();
+    if (file.length > 0 && file.length < 7) {
+      const promies = [];
+      for (let i = 0; i < file.length; i++) {
+        promies.push(storeImage(file[i]));
+      }
+
+      Promise.all(promies)
+        .then((urls) => {
+          setFormData({
+            ...formdata,
+            imagesUrl: urls,
+          });
+          setUploadImage(false);
+          setUploadImageError(false);
+        })
+        .catch((error) => {
+          setUploadImage(false);
+          setUploadImageError("image upload failed (2mb max per image)");
+        });
+    } else {
+      setUploadImage(false);
+      setUploadImageError("image upload failed (2mb max per image)");
+    }
+  };
+
+  const storeImage = async (image) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + image.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, image);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+          console.log('progress : ',progress);
+          setProgress(progress);
+
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const deleteImage = (index) => {
+    setFormData({
+      ...formdata,
+      imagesUrl: formdata.imagesUrl.filter((_, i) => i !== index),
+    });
+  };
   return (
     <main className="max-w-4xl p-3 mx-auto">
       <h1 className="text-3xl text-center my-7 font-bold">Create a Listing</h1>
@@ -10,8 +94,6 @@ export default function CreateListining() {
             type="text"
             id="name"
             placeholder="Name"
-            maxlength="62"
-            minlength="10"
             required=""
             className="border p-3 rounded-lg"
             value=""
@@ -92,7 +174,7 @@ export default function CreateListining() {
                 <p>Regular price </p>
                 <span className="text-xs">($ / Month)</span>
               </div>
-            </div>s
+            </div>
           </div>
         </div>
         <div className="flex flex-col flex-1 gap-4">
@@ -102,21 +184,22 @@ export default function CreateListining() {
               The first image will be the cover (max 6)
             </span>
           </p>
-          <form className="flex gap-4">
+          <div className="flex gap-4">
             <input
               type="file"
               id="images"
               accept="image/*"
-              multiple=""
+              onChange={(e) => setFile(e.target.files)}
+              multiple
               className="p-3 border border-gray-300 rounded w-full"
             />
             <button
-              type="submit"
+              onClick={(e) => handleImageUpload(e)}
               className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
             >
-              Upload
+              {uploadImage ? "Uploading... " : "Upload"}
             </button>
-          </form>
+          </div>
           <p className="text-red-700"></p>
           <button
             type="submit"
@@ -124,6 +207,24 @@ export default function CreateListining() {
           >
             Create Listing
           </button>
+          <p className="text-red-700"> {uploadImageError}</p>
+          {formdata.imagesUrl.length > 0 &&
+            formdata.imagesUrl.map((url, i) => (
+              <div key={i} className="flex justify-between p-3  items-center  ">
+                <img
+                  key={i}
+                  src={url}
+                  alt=""
+                  className="w-20 h-20 object-cover rounded-lg"
+                />
+                <button
+                  onClick={() => deleteImage(i)}
+                  className="text-red-700 uppercase hover:opacity-60 rounded-lg"
+                >
+                  delete
+                </button>
+              </div>
+            ))}
         </div>
       </form>
     </main>
